7fb5b4fe9fba5a298e63ef9d460eeb2548b0bc86
Pulkit Rastogi <pulkit_rastogi@persistent.co.in>
2020-03-11 16:31:57 +0530

    LAst commit

--- 09-03-2020/IntermmediateSpringWorkSpace/SpringTransactionManagementAnnotation/src/com/psl/transaction/Client.java
+++ 09-03-2020/IntermmediateSpringWorkSpace/SpringTransactionManagementAnnotation/src/com/psl/transaction/Client.java
@@ -1,6 +1,7 @@
 package com.psl.transaction;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
@@ -10,5 +11,9 @@ public class Client {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 		MyInterface ref = (MyInterface)ctx.getBean("cls1");
 		ref.method1();
+		
+		((AbstractApplicationContext)ctx).close();
+		//contextRoute - A name
+		
 	}
 }
